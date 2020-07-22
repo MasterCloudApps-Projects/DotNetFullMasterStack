2bd11dcf0f1c9ba4f0177c82c5ab0277325085ba
Javier Vela <fvelaayl@everis.com>
2020-07-22 17:30:50 +0200

    added models in kitchen api

---
+++ src/Services/Kitchen/Kitchen.API/Models/TicketLine.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kitchen.API.Models
+{
+    public class TicketLine
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
