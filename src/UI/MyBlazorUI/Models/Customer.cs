ba378b042271da03bcb687c8a8a2069794feb19a
Javier Vela <fjvela@gmail.com>
2020-11-30 21:09:53 +0100

    added asp.net blazor ui

---
+++ src/UI/MyBlazorUI/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace MyBlazorUI.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
